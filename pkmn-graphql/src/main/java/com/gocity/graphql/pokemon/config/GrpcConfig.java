4cd14b3ab620fac491718b4bd29bc8d6489ae35f
Samuel Woodbridge <samuel.woodbridge@gocity.com>
2022-11-27 00:09:18 +0000

    [Structure] - Renames

--- pkmn-graphql/src/main/java/com/gocity/graphql/pokemon/config/GrpcConfig.java
+++ pkmn-graphql/src/main/java/com/gocity/graphql/pokemon/config/GrpcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GRCPConfig {
+public class GrpcConfig {
 
     @Bean(name = "pkmn-channel")
     Channel pokemonChannel() {
